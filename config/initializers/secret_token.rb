7dbe46d22b751d8d73497d240d9a33b53e2b161d
Jim Knight <jim.knight@lavatech.com>
2012-06-03 20:03:50 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serverup::Application.config.secret_token = '18e1848fef20af8ea9d92f4b8bce821c4646e19bfd4305c46149fad8660424853d5a74840d31c7bc27db190c584494e23ea8753698170200e9ab46b087bac729'
